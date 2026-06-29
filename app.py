967be2d8bb1c3fb00500ba94c338bf8bbbc8597b
Globalaviationsa <atsaktanis@globalaviationsa.com>
2026-06-29 13:59:31 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ TEMPLATE_MAP = {
     "Sep_2025": "Application ATPL Fillable_Sep_2025_3rd_Trimester.pdf",
     "Oct_2025": "Application ATPL Fillable_Oct_2025_3rd_Trimester.pdf",
     "Jan_2026": "Application ATPL Fillable_Jan_2026_2nd_Trimester.pdf",
-    "Mar_2026": "Application ATPL Fillable_Mar_2026_1st_Trimester.pdf",
-    "Mar_A3_2026": "Application ATPL Fillable_Aegean_2026_1st_Trimester.pdf",
-    "May_2026": "Application ATPL Fillable_May_2026_1st_Trimester.pdf"
+    "Mar_2026": "Application ATPL Fillable_Mar_2026_2nd_Trimester.pdf",
+    "Mar_A3_2026": "Application ATPL Fillable_Aegean_2026_2nd_Trimester.pdf",
+    "May_2026": "Application ATPL Fillable_May_2026_1st_Trimester.pdf",
+    "Jun_2026": "Application ATPL Fillable_June_2026_1st_Trimester.pdf"
 }
 OUTPUT_DIR = "filled_pdfs"
 
